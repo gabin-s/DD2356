43118498c0fb25d62796ca35d4fac502a4f293ec
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-10 15:40:04 +0200

    adapted run script for II.3

--- Assignment-II/run_dftw.sh
+++ Assignment-II/run_dftw.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-for i in $(seq 100); do
-    OMP_NUM_THREADS=32 ./dftw | grep seconds | awk '{print $4}'
+for nthreads in 1 2 4 8 12 16 20 24 28 32; do
+    for i in $(seq 10); do
+        echo -n "$nthreads "
+        OMP_NUM_THREADS=$nthreads ./dftw | grep seconds | awk '{print $4}'
+    done
 done
