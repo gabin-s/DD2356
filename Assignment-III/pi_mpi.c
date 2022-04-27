267a0ab2e4c251cfc0fec2a2470c181c83e4746a
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-27 14:06:40 +0200

    changed to Irecv

--- Assignment-III/pi_mpi.c
+++ Assignment-III/pi_mpi.c
@@ -40,17 +40,21 @@ int main(int argc, char* argv[])
     }    
 
     if(rank == 0) {
-        int val;
-        long total_count = local_count;
+        int counts[size - 1];
+        MPI_Request requests[size - 1];
 
         for(int i = 1; i < size; i++) {
-            MPI_Recv(&val, 1, MPI_INT, /* src */ i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            total_count += val;
+            MPI_Irecv(&counts[i - 1], 1, MPI_INT, i, TAG, MPI_COMM_WORLD, &requests[i - 1]);
         }
+        MPI_Waitall(size - 1, requests, MPI_STATUSES_IGNORE);
 
+        // sum all results
+        long total_count = local_count;
+        for(int i = 0; i < size - 1; i++) {
+            total_count += counts[i];
+        }
         // Estimate Pi and display the result
         pi = ((double)total_count / (double)NUM_ITER) * 4.0;
-
     }
     else {
         MPI_Send(&local_count, 1, MPI_INT, /* dest */ 0, TAG, MPI_COMM_WORLD);
