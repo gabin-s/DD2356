5626289f441084a323c1ff66c3d83411f8ae07e0
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-27 09:00:48 +0200

    fixed

--- Assignment-III/ping_pong.c
+++ Assignment-III/ping_pong.c
@@ -48,8 +48,7 @@ int main(int argc, char *argv[])
 		
 		MPI_Win win;
 		int soi = sizeof(int);
-		if(rank == 1)
-  			MPI_Win_create(A, soi*N, soi, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+		MPI_Win_create(A, soi*N, soi, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
 
 		// Warm-up loop
 		for(int i=1; i<=5; i++){
@@ -82,8 +81,7 @@ int main(int argc, char *argv[])
 		if(rank == 0) printf("%10li\t%15.9f\n", num_B, avg_time_per_transfer);
 
 
-		if(rank == 1)
-			MPI_Win_free(&win);
+		MPI_Win_free(&win);
 		free(A);
 	}
 
