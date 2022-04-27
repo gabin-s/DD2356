ef81848a13772cb2e4b0eb4f885608d24fba88ba
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-27 09:04:47 +0200

    fixed

--- Assignment-III/ping_pong.c
+++ Assignment-III/ping_pong.c
@@ -50,29 +50,30 @@ int main(int argc, char *argv[])
 		int soi = sizeof(int);
 		if(rank == 1)
 			MPI_Win_create(A, soi*N, soi, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+		else {
+			// Warm-up loop
+			for(int i=1; i<=5; i++){
+				MPI_Win_fence(0, win);
+				MPI_Get(A, N, MPI_DOUBLE, 1,
+						0, N, MPI_DOUBLE, win);
+				MPI_Win_fence(0, win); //Sync to make sure the get is complete
+			}
 
-		// Warm-up loop
-		for(int i=1; i<=5; i++){
-			MPI_Win_fence(0, win);
-			MPI_Get(A, N, MPI_DOUBLE, 0,
-					0, N, MPI_DOUBLE, win);
-			MPI_Win_fence(0, win); //Sync to make sure the get is complete
+			// Time ping-pong for loop_count iterations of data transfer size 8*N bytes
+
+			double start_time, stop_time, elapsed_time;
+			start_time = MPI_Wtime();
+
+			for(int i=1; i<=loop_count; i++){
+				MPI_Win_fence(0, win);
+				MPI_Get(A, N, MPI_DOUBLE, 0,
+						0, N, MPI_DOUBLE, win);
+				MPI_Win_fence(0, win); //Sync to make sure the get is complete
+			}
+
+			stop_time = MPI_Wtime();
+			elapsed_time = stop_time - start_time;
 		}
-
-		// Time ping-pong for loop_count iterations of data transfer size 8*N bytes
-
-		double start_time, stop_time, elapsed_time;
-		start_time = MPI_Wtime();
-
-		for(int i=1; i<=loop_count; i++){
-			MPI_Win_fence(0, win);
-			MPI_Get(A, N, MPI_DOUBLE, 0,
-					0, N, MPI_DOUBLE, win);
-			MPI_Win_fence(0, win); //Sync to make sure the get is complete
-		}
-
-		stop_time = MPI_Wtime();
-		elapsed_time = stop_time - start_time;
 
 		long int num_B = 8*N;
 		long int B_in_GB = 1 << 30;
