6331a3cc43018ab88be375a8bfdde8be273b320f
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-27 13:45:15 +0200

    improved run script

--- Assignment-III/run_pi_mpi.sh
+++ Assignment-III/run_pi_mpi.sh
@@ -2,5 +2,6 @@
 
 for task_number in 8 16 32 64 128; do
 	echo -n "$task_number "
-	srun -n $task_number ./pi_mpi | awk -F "=" "{print $3}"
+	srun -n $task_number ./pi_mpi | awk -F "=" "{ print $3}"
+	[ $? -ne 0 ] && break
 done;
