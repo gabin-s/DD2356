2f911668a34ff7bebbb63b8280d79bbd36b92e77
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-28 01:23:27 +0200

    debug

--- Assignment-III/bonus/fox.c
+++ Assignment-III/bonus/fox.c
@@ -114,7 +114,7 @@ void mpi_matmul(int M, int s_N, int rank, number_t *A, number_t *B) {
     tile_copy(m, s_N, rank, A_tile, A);
     tile_copy(m, s_N, rank, B_tile, B);
 
-    printf("r%d, A=%f, B=%f, C=%f\n", rank, *A_tile, *B_tile, *C_tile);
+    printf("r%d, m=%d, s_N=%d, A=%f, B=%f, C=%f\n", m, s_N, rank, *A_tile, *B_tile, *C_tile);
     print_matrix(m, A_tile);
 
     // create a new communicator for the row
