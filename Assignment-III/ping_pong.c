3544e9e2a817f3ab8b19fea5f6ccd36b3971822c
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-27 09:43:53 +0200

    clarified code

--- Assignment-III/ping_pong.c
+++ Assignment-III/ping_pong.c
@@ -50,47 +50,36 @@ int main(int argc, char *argv[])
 		int sod = sizeof(double);
 		MPI_Win_create(A, sod*N, sod, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
 
-		if(rank == 0) {
-			// Warm-up loop
-			for(int i=1; i<=5; i++){
-				MPI_Win_fence(0, win);
-				MPI_Get(A, N, MPI_DOUBLE, 1,
-						0, N, MPI_DOUBLE, win);
-				MPI_Win_fence(0, win);
-			}
+		// Warm-up loop
+		for(int i=1; i<=5; i++){
+			MPI_Win_fence(0, win);
+			MPI_Get(A, N, MPI_DOUBLE, 1-rank,
+					0, N, MPI_DOUBLE, win);
+			MPI_Win_fence(0, win);
+		}
 
-			// Time ping-pong for loop_count iterations of data transfer size 8*N bytes
+		// Time ping-pong for loop_count iterations of data transfer size 8*N bytes
 
-			double start_time, stop_time, elapsed_time;
-			start_time = MPI_Wtime();
+		double start_time, stop_time, elapsed_time;
+		start_time = MPI_Wtime();
 
-			for(int i=1; i<=loop_count; i++){	
-				MPI_Win_fence(0, win);
-				MPI_Get(A, N, MPI_DOUBLE, 1,
-						0, N, MPI_DOUBLE, win);
-				MPI_Win_fence(0, win);
-			}
+		for(int i=1; i<=loop_count; i++){	
+			MPI_Win_fence(0, win);
+			MPI_Get(A, N, MPI_DOUBLE, 1-rank,
+					0, N, MPI_DOUBLE, win);
+			MPI_Win_fence(0, win);
+		}
 
-			stop_time = MPI_Wtime();
-			elapsed_time = stop_time - start_time;
+		stop_time = MPI_Wtime();
+		elapsed_time = stop_time - start_time;
 
-			long int num_B = 8*N;
-			long int B_in_GB = 1 << 30;
-			double num_GB = (double)num_B / (double)B_in_GB;
-			double avg_time_per_transfer = elapsed_time / (2.0*(double)loop_count);
+		long int num_B = 8*N;
+		long int B_in_GB = 1 << 30;
+		double num_GB = (double)num_B / (double)B_in_GB;
+		double avg_time_per_transfer = elapsed_time / (2.0*(double)loop_count);
 
+		if(rank == 0)
 			printf("%10li\t%15.9f\n", num_B, avg_time_per_transfer);
-		}
-		else if (rank == 1) {
-			for(int i = 1; i <= 5; i++) {
-				MPI_Win_fence(0, win); 	// synchronize
-				MPI_Win_fence(0, win); 	// synchronize
-			}
-			for(int i = 1; i <= loop_count; i++) {
-				MPI_Win_fence(0, win); 	// synchronize
-				MPI_Win_fence(0, win); 	// synchronize
-			}
-		}
 
 		MPI_Win_free(&win);
 		free(A);
