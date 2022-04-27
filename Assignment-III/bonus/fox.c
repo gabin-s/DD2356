9e1b2a43f52f5dc41c4d45a7f624ee87da918907
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-04-28 01:19:50 +0200

    debug

--- Assignment-III/bonus/fox.c
+++ Assignment-III/bonus/fox.c
@@ -115,7 +115,7 @@ void mpi_matmul(int M, int s_N, int rank, number_t *A, number_t *B) {
     tile_copy(m, s_N, rank, B_tile, B);
 
     printf("r%d, A=%f, B=%f, C=%f\n", rank, *A_tile, *B_tile, *C_tile);
-    print_matrix(m, A);
+    print_matrix(m, A_tile);
 
     // create a new communicator for the row
     div_t colorkey = div(rank, s_N);
