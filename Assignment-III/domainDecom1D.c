9b92166f06eb2dd852948b7a50053532e0489d5f
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-05-16 18:05:34 +0200

    added missing domainDecomp

--- Assignment-III/domainDecom1D.c
+++ Assignment-III/domainDecom1D.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define TAG 123
+
 int main(int argc, char *argv[]){
 
     int rank, size, i, provided;
@@ -12,7 +14,6 @@ int main(int argc, char *argv[]){
     int nxc = 128; // make sure nxc is divisible by size
     double L = 2*3.1415; // Length of the domain
     
-
     MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -24,34 +25,41 @@ int main(int argc, char *argv[]){
     double dx = L / ((double) nxc);
     
     // define out function
-    double *f = calloc(nxn_loc, sizeof(double)); // allocate and fill with z
-    double *dfdx = calloc(nxn_loc, sizeof(double)); // allocate and fill with z
+    double *f    = malloc(nxn_loc * sizeof(double));
+    double *dfdx = malloc(nxn_loc * sizeof(double));
 
     for (i=1; i<(nxn_loc-1); i++)
-      f[i] = sin(L_loc*rank + (i-1) * dx);
+        f[i] = sin(L_loc*rank + (i-1) * dx);
     
-    // need to communicate and fill ghost cells f[0] and f[nxn_loc-1]
-    // communicate ghost cells
-    // ...
-    // ...  
+    // communicate and fill ghost cells f[0] and f[nxn_loc-1]
+    int next = (rank == size - 1) ? 0 : (rank + 1);
+    int prev  = (rank == 0) ? (size - 1):(rank - 1);
 
+    // send f[2] to f[nxn_loc - 1] of the previous rank (send left, receive right)
+    MPI_Sendrecv(&f[2], 1, MPI_DOUBLE, prev, TAG, 
+                 &f[nxn_loc-1], 1, MPI_DOUBLE, next, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    // send f[nxn_loc-3] to f[0] of the next rank (send right, receive left)
+    MPI_Sendrecv(&f[nxn_loc-3], 1, MPI_DOUBLE, next, TAG, 
+                 &f[0], 1, MPI_DOUBLE, prev, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     // here we finish the calculations
 
     // calculate first order derivative using central difference
     // here we need to correct value of the ghost cells!
-    for (i=1; i<(nxn_loc-1); i++)
-      dfdx[i] = (f[i+1] - f[i-1])/(2*dx);
 
+    for (i=1; i<(nxn_loc-1); i++)
+        dfdx[i] = (f[i+1] - f[i-1])/(2*dx);
+
+    if (rank==0) // print only rank 0 for convenience
+        fprintf(stderr, "My rank %d of %d\n", rank, size );
     
-    // Print f values
-    if (rank==0){ // print only rank 0 for convenience
-        printf("My rank %d of %d\n", rank, size );
-        printf("Here are my values for f including ghost cells\n");
-        for (i=0; i<nxn_loc; i++)
-	       printf("%f\n", f[i]);
-        printf("\n");
-    }   
+    // print x and y, stop at n-3 to avoid duplicate points
+    for (i=1; i<nxn_loc-2; i++)
+	    printf("%f %f\n", L_loc*rank + (i-1) * dx, dfdx[i]);
+    
+    free(f);
+    free(dfdx);
 
     MPI_Finalize();
 }
