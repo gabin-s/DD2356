678a7ab2e8a2285a9f6f1bff2b09cc4733656b41
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-05-25 15:35:46 +0200

    added values in run_nsolver

--- Assignment-I/n-body-solver/run_nsolver.sh
+++ Assignment-I/n-body-solver/run_nsolver.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-N_RUNS=50
+N_RUNS=10
 
 # constant parameters for ./nsolver
 dt=0.05
 n_steps=100
 
-for npart in 500 1000 2000; do
+for npart in 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000; do
     echo -n "$npart"
     for i in $(seq $N_RUNS); do
         out=$(./nsolver $npart $dt $n_steps)
